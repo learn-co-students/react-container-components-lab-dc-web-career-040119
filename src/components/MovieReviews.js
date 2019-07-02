b7145bc50dfd5a7af646ee27c1d0c5d1161f2299
github username <github email address>
2019-07-02 09:53:23 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
-// Code MovieReviews Here
+import React, {Component, Fragment} from 'react'
+
+const MovieReviews = (props) => {
+  return(<>
+            <ul>
+              {props.results.map(review => <React.Fragment> key={review.display_title}>
+                <h4>{review.headline}</h4>
+                <p>{review.summary_short}</p>
+              <a href='{review.link.url}'>{review.suggested_link_text}</a> </React.Fragment>
+            </ul>
+        </>)
+}
+
+export default MovieReviews
